7c9d8cc2f814d8424e4525689c22a63fa2b52150
ChainsAre2Tight <dima124691@gmail.com>
2026-04-12 22:12:49 +0300

    adds warning timeline

--- src/components/IncidentDetailsPanel/WarningTimeline.tsx
+++ src/components/IncidentDetailsPanel/WarningTimeline.tsx
@@ -1,11 +1,34 @@
+import { useAppStore } from "../../store/useAppStore"
+import { buildWarningAdapter } from "../../timeline/adapters/warnings"
+import TimelineCanvas from "../../timeline/components/TimelineCanvas"
 
 export default function WarningTimeline() {
 
+    const incidentId = useAppStore(s => s.selection.incidentId)
+    const incidentsByModality = useAppStore(s => s.report.incidentsByModality)
+
+    const incident = Object.values(incidentsByModality)
+        .flat()
+        .find(i => i.id === incidentId)
+
+    if (!incident) return
+
+    const adapter = buildWarningAdapter({
+        incident,
+    })
+
     return (
+        <TimelineCanvas
+            adapter={adapter}
 
-        <div style={{ width: "100%", height: "100%" }}>
-            123
-        </div>
+            externalSelectedKey={null}
+            onSelect={() => {}}
 
+            externalHoverKey={null}
+            externalHoverTimeUs={null}
+
+            onHoverItem={() => {}}
+            onHoverTime={() => {}}
+        />
     )
 }
