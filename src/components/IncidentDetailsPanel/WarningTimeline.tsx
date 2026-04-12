713912dca5c2ca7ae169a66f1fb7f2414dcf00d7
ChainsAre2Tight <dima124691@gmail.com>
2026-04-12 22:28:31 +0300

    adds warning timeline hover

--- src/components/IncidentDetailsPanel/WarningTimeline.tsx
+++ src/components/IncidentDetailsPanel/WarningTimeline.tsx
@@ -3,15 +3,22 @@ import { buildWarningAdapter } from "../../timeline/adapters/warnings"
 import TimelineCanvas from "../../timeline/components/TimelineCanvas"
 
 export default function WarningTimeline() {
-
     const incidentId = useAppStore(s => s.selection.incidentId)
     const incidentsByModality = useAppStore(s => s.report.incidentsByModality)
+
+    const warningKey = useAppStore(s => s.selection.warningKey)
+    const hoveredWarningKey = useAppStore(s => s.hover.warningKey)
+    const hoverTimeUs = useAppStore(s => s.hover.timelineTimeUs)
+
+    const selectWarning = useAppStore(s => s.selectWarning)
+    const hoverWarning = useAppStore(s => s.hoverWarning)
+    const setTimelineCursor = useAppStore(s => s.setTimelineCursor)
 
     const incident = Object.values(incidentsByModality)
         .flat()
         .find(i => i.id === incidentId)
 
-    if (!incident) return
+    if (!incident) return null
 
     const adapter = buildWarningAdapter({
         incident,
@@ -21,14 +28,28 @@ export default function WarningTimeline() {
         <TimelineCanvas
             adapter={adapter}
 
-            externalSelectedKey={null}
-            onSelect={() => {}}
+            externalSelectedKey={warningKey}
+            onSelect={(item) => {
+                if (!item) {
+                    selectWarning(null)
+                    return
+                }
 
-            externalHoverKey={null}
-            externalHoverTimeUs={null}
+                if (item.type === "warning") {
+                    selectWarning(item.key)
+                }
+            }}
 
-            onHoverItem={() => {}}
-            onHoverTime={() => {}}
+            externalHoverKey={hoveredWarningKey}
+            externalHoverTimeUs={hoverTimeUs}
+
+            onHoverItem={(item) => {
+                hoverWarning(item?.key ?? null)
+            }}
+
+            onHoverTime={(timeUs) => {
+                setTimelineCursor(timeUs)
+            }}
         />
     )
 }
